2d8a977a6a01957f0b78383fe9098174feabeffb
Huzaifa <huzaifa.unjha@gmail.com>
2025-07-01 08:00:38 -0400

    some more micro opt in eq Plane

--- geometry/proximity/sycl/utils/sycl_equilibrium_plane.h
+++ geometry/proximity/sycl/utils/sycl_equilibrium_plane.h
@@ -44,70 +44,66 @@ SYCL_EXTERNAL inline bool ComputeEquilibriumPlane(
     double gradP_A_Wo_x, double gradP_A_Wo_y, double gradP_A_Wo_z,
     double p_A_Wo, double gradP_B_Wo_x, double gradP_B_Wo_y,
     double gradP_B_Wo_z, double p_B_Wo, double* eq_plane_out) {
-  // Compute n_W = grad_f0_W - grad_f1_W
   const double n_W_x = gradP_A_Wo_x - gradP_B_Wo_x;
   const double n_W_y = gradP_A_Wo_y - gradP_B_Wo_y;
   const double n_W_z = gradP_A_Wo_z - gradP_B_Wo_z;
-  double n_W_norm = n_W_x * n_W_x + n_W_y * n_W_y + n_W_z * n_W_z;
 
-  if (n_W_norm <= 0.0) {
+  const double n_W_norm_sq = n_W_x * n_W_x + n_W_y * n_W_y + n_W_z * n_W_z;
+  const double gradP_A_W_norm_sq = gradP_A_Wo_x * gradP_A_Wo_x +
+                                   gradP_A_Wo_y * gradP_A_Wo_y +
+                                   gradP_A_Wo_z * gradP_A_Wo_z;
+  const double gradP_B_W_norm_sq = gradP_B_Wo_x * gradP_B_Wo_x +
+                                   gradP_B_Wo_y * gradP_B_Wo_y +
+                                   gradP_B_Wo_z * gradP_B_Wo_z;
+
+  // Early exit if the normal is zero
+  if (n_W_norm_sq <= 0.0) {
     return false;
   }
-  const double n_W_inv_norm = sycl::rsqrt(n_W_norm);
-  const double n_W_x_normalized = n_W_x * n_W_inv_norm;
-  const double n_W_y_normalized = n_W_y * n_W_inv_norm;
-  const double n_W_z_normalized = n_W_z * n_W_inv_norm;
 
-  // Normalized pressure gradient for A
-  const double gradP_A_W_norm = gradP_A_Wo_x * gradP_A_Wo_x +
-                                gradP_A_Wo_y * gradP_A_Wo_y +
-                                gradP_A_Wo_z * gradP_A_Wo_z;
-  const double gradP_A_W_inv_norm = sycl::rsqrt(gradP_A_W_norm);
-  const double gradP_A_W_normalized_x = gradP_A_Wo_x * gradP_A_W_inv_norm;
-  const double gradP_A_W_normalized_y = gradP_A_Wo_y * gradP_A_W_inv_norm;
-  const double gradP_A_W_normalized_z = gradP_A_Wo_z * gradP_A_W_inv_norm;
-  const double cos_theta_A = n_W_x_normalized * gradP_A_W_normalized_x +
-                             n_W_y_normalized * gradP_A_W_normalized_y +
-                             n_W_z_normalized * gradP_A_W_normalized_z;
+  // Pipeline reciprocal square roots
+  const double n_W_inv_norm = sycl::rsqrt(n_W_norm_sq);
+  const double gradP_A_W_inv_norm = sycl::rsqrt(gradP_A_W_norm_sq);
+  const double gradP_B_W_inv_norm = sycl::rsqrt(gradP_B_W_norm_sq);
 
-  constexpr double kAlpha = 5. * M_PI / 8.;
+  // Conditional checks
+  const double dot_n_gA =
+      n_W_x * gradP_A_Wo_x + n_W_y * gradP_A_Wo_y + n_W_z * gradP_A_Wo_z;
+  const double dot_n_gB =
+      n_W_x * gradP_B_Wo_x + n_W_y * gradP_B_Wo_y + n_W_z * gradP_B_Wo_z;
+
+  const double cos_theta_A = dot_n_gA * n_W_inv_norm * gradP_A_W_inv_norm;
+
+  constexpr double kAlpha = 5.0 * M_PI / 8.0;
   const double kCosAlpha = sycl::cos(kAlpha);
 
   if (cos_theta_A <= kCosAlpha) {
     return false;
   }
 
-  // Normalized pressure gradient for B
-  const double gradP_B_W_norm = gradP_B_Wo_x * gradP_B_Wo_x +
-                                gradP_B_Wo_y * gradP_B_Wo_y +
-                                gradP_B_Wo_z * gradP_B_Wo_z;
-  const double gradP_B_W_inv_norm = sycl::rsqrt(gradP_B_W_norm);
-  const double gradP_B_W_normalized_x = gradP_B_Wo_x * gradP_B_W_inv_norm;
-  const double gradP_B_W_normalized_y = gradP_B_Wo_y * gradP_B_W_inv_norm;
-  const double gradP_B_W_normalized_z = gradP_B_Wo_z * gradP_B_W_inv_norm;
-  const double cos_theta_B = -n_W_x_normalized * gradP_B_W_normalized_x +
-                             -n_W_y_normalized * gradP_B_W_normalized_y +
-                             -n_W_z_normalized * gradP_B_W_normalized_z;
+  const double cos_theta_B = -dot_n_gB * n_W_inv_norm * gradP_B_W_inv_norm;
 
   if (cos_theta_B <= kCosAlpha) {
     return false;
   }
 
-  // gM corresponds to the dot product of gradient for object A with the normal
-  const double gM = gradP_A_Wo_x * n_W_x_normalized +
-                    gradP_A_Wo_y * n_W_y_normalized +
-                    gradP_A_Wo_z * n_W_z_normalized;
+  const double n_W_x_normalized = n_W_x * n_W_inv_norm;
+  const double n_W_y_normalized = n_W_y * n_W_inv_norm;
+  const double n_W_z_normalized = n_W_z * n_W_inv_norm;
 
-  // gN corresponds to the negative dot product of gradient for object B with
-  // the normal
-  const double gN =
-      -(gradP_B_Wo_x * n_W_x_normalized + gradP_B_Wo_y * n_W_y_normalized +
-        gradP_B_Wo_z * n_W_z_normalized);
+  const double gM = dot_n_gA * n_W_inv_norm;
+  const double gN = -dot_n_gB * n_W_inv_norm;
 
-  // Plane point
-  double p_WQ_x = ((p_B_Wo - p_A_Wo) * n_W_inv_norm) * n_W_x_normalized;
-  double p_WQ_y = ((p_B_Wo - p_A_Wo) * n_W_inv_norm) * n_W_y_normalized;
-  double p_WQ_z = ((p_B_Wo - p_A_Wo) * n_W_inv_norm) * n_W_z_normalized;
+  // Plane point calculation
+  const double p_diff = p_B_Wo - p_A_Wo;
+  const double n_W_norm_sq_rcp = n_W_inv_norm * n_W_inv_norm;
+  const double scale = p_diff * n_W_norm_sq_rcp;
+
+  const double p_WQ_x = scale * n_W_x;
+  const double p_WQ_y = scale * n_W_y;
+  const double p_WQ_z = scale * n_W_z;
+
+  // Store final results
   eq_plane_out[0] = n_W_x_normalized;
   eq_plane_out[1] = n_W_y_normalized;
   eq_plane_out[2] = n_W_z_normalized;
@@ -116,9 +112,9 @@ SYCL_EXTERNAL inline bool ComputeEquilibriumPlane(
   eq_plane_out[5] = p_WQ_z;
   eq_plane_out[6] = gM;
   eq_plane_out[7] = gN;
+
   return true;
 }
-
 }  // namespace sycl_impl
 }  // namespace internal
 }  // namespace geometry
